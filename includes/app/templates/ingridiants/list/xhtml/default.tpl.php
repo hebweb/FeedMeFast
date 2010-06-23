ad65c7c41b9d11df0b8ce8c9990999559ef79d01
Arieh <Arieh@Arieh-Laptop>
2010-06-23 11:52:24 +0300

    added html markup to ingridiants listing add htc file

--- includes/app/templates/ingridiants/list/xhtml/default.tpl.php
+++ includes/app/templates/ingridiants/list/xhtml/default.tpl.php
@@ -1,2 +1,20 @@
-<?php
-var_dump($this->model->getIngridiants());
+<h1>רשימת רכיבים</h1>
+<div id='content'>
+<ol>
+<?php foreach ($this->model->getIngridiants() as $ing):?>
+    <li>    	<dl>    		<dt>שם:</dt>    		<dd><?php echo $ing['name']?></dd>
+    		
+    		<dt>מתאים למתכונים חמים?</dt>
+    		<dd><?php echo $ing['hot'] ? 'כן' : 'לא'?></dd>
+    		
+    		<dt>מתאים למתכונים קרים?</dt>
+    		<dd><?php echo $ing['cold'] ? 'כן' : 'לא'?></dd>
+    		
+    		<?php if ($ing['manufactors']):?>
+        		<dt>יצרנים:</dt>        		<?php foreach ($ing['manufactors'] as $manu):?>
+        		  <dd><?php echo $manu['name'];?></dd>
+        		<?php endforeach;?>
+            <?php endif;?>    	</dl>    </li>
+<?php endforeach;?>
+</ol>
+</div>
